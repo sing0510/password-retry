d1dd9d08d1421b01ee6d3ccb22c9c9991ce72619
Sing Heung <singwai0510@gmail.com>
2020-01-08 17:20:33 +0800

    password verson done

--- password_retry.py
+++ password_retry.py
@@ -1,15 +1,16 @@
 password = ('singsing')
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input ('請輸入密碼')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('錯誤,還有',i,'機會')
-		if i == 0:
-			print('登入失敗')
+		if i > 0:
+			print('錯誤,還有',i,'機會')
+		else:
+			print('沒機會了')
 
 
 
